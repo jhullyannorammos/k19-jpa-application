2b4ab13e3d74a0cee4ba18f3d60e679bd07004ed
Juliano Ramos <jhullyannoors@gmail.com>
2022-07-17 21:05:46 -0300

    Criando metodos e filtros em PessoaRepositoryImplementation.java

--- src/main/java/br/com/application/repository/implementation/PessoaRepositoryImplementation.java
+++ src/main/java/br/com/application/repository/implementation/PessoaRepositoryImplementation.java
@@ -3,6 +3,7 @@ package br.com.application.repository.implementation;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.persistence.LockModeType;
 import javax.persistence.PersistenceContext;
 import javax.persistence.TypedQuery;
 
@@ -12,6 +13,38 @@ import br.com.application.model.Pessoa;
 import br.com.application.repository.PessoaRepository;
 
 public class PessoaRepositoryImplementation implements PessoaRepository{
+	
+	String SQLSome = " SELECT Pessoa FROM Pessoa Pessoa WHERE Pessoa.nome = SOME(SELECT Pessoa2.titulo FROM Pessoa Pessoa2 WHERE Pessoa <> Pessoa2)";
+	String SQLNot = " SELECT f FROM Funcionario f WHERE f NOT MEMBER OF f. empresa . diretoria ";
+	String SQLWhere = " SELECT f FROM Funcionario f WHERE f MEMBER OF f. empresa . diretoria ";
+	String SQLWhereNot = " SELECT p FROM Pessoa p WHERE NOT (p. idade >= : idade )";
+	String SQLIsNotNull = " SELECT p FROM Pessoa p WHERE p.nome IS NOT NULL OR p. idade >= : idade ";
+	String SQLWhereNotGreatestThan = "SELECT p FROM Pessoa p WHERE p.idade <: idade";
+	String SQLWhereEqual = " SELECT p FROM Pessoa p WHERE p.idade <> : idade ";
+	String SQLWhereNotGreatestOrEqualThan = " SELECT p FROM Pessoa p WHERE p.idade > : idade ";
+	String SQLWhereGreatestOrEqualThan = " SELECT p FROM Pessoa p WHERE p.idade <= : idade ";
+	String SQLWhereGreatestAndEqualThan = " SELECT p FROM Pessoa p WHERE p.idade >= : idade ";
+	String SQLWhereEqualThan = " SELECT p FROM Pessoa p WHERE p.idade = : idade ";
+	String SQLWhereIsNull = " SELECT p FROM Pessoa p WHERE p.nome IS NULL ";
+	String SQLWhereIsNotNull = " SELECT p FROM Pessoa p WHERE p.nome IS NOT NULL ";
+	String SQLBetween = " SELECT p FROM Pessoa p WHERE p.idade BETWEEN : minimo AND : maximo ";
+	String SQLNotBetween = " SELECT p FROM Pessoa p WHERE p.idade NOT BETWEEN : minimo AND : maximo ";
+	String SQLIsNotNullAnd = " SELECT p FROM Pessoa p WHERE p.nome IS NOT NULL AND p. idade >= : idade ";
+	String SQLisNotEmpty = " SELECT a FROM Pessoa a WHERE a.Pessoas IS NOT EMPTY ";
+	String SQLisEmpty = " SELECT a FROM Pessoa a WHERE a.Pessoas IS EMPTY ";
+	String SQLExists = " SELECT d FROM Departamento d WHERE EXISTS(SELECT f FROM FUNCIONARIO f WHERE f. departamento = d)";
+	String SQLNotExists = " SELECT d FROM Departamento d WHERE NOT EXISTS(SELECT f FROM FUNCIONARIO f WHERE f. departamento = d)";
+	String SQLNotLike = " SELECT a FROM Pessoa a WHERE a.nome NOT LIKE Patrick %";
+	String SQLike = " SELECT a FROM Pessoa a WHERE a.nome LIKE Patrick %";
+	String SQLin = " SELECT a FROM Pessoa a WHERE a.nome IN ( ’ Patrick Cullen ’, ’Fraser Seitel ’)";
+	String SQLnotIn = " SELECT a FROM Pessoa a WHERE a.nome NOT IN ( ’ Patrick Cullen ’, ’Fraser Seitel ’)";
+	String SQLAll = " SELECT Pessoa FROM Pessoa Pessoa WHERE Pessoa.preco >= ALL( SELECT Pessoa.preco FROM Pessoa Pessoa )";
+	String SQLany = " SELECT Pessoa FROM Pessoa Pessoa WHERE Pessoa.nome = ANY( SELECT Pessoa2.titulo FROM Pessoa Pessoa2 WHERE Pessoa <> Pessoa2 )";
+	String SQLavg = " SELECT AVG(Pessoa.preco) FROM Pessoa Pessoa ";
+	String SQLSum = " SELECT SUM(Pessoa.preco) FROM Pessoa Pessoa ";
+	String SQLMin = " SELECT MIN(Pessoa.preco) FROM Pessoa Pessoa ";
+	String SQLMax = " SELECT MAX(Pessoa.preco) FROM Pessoa Pessoa ";
+	String SQLCount = " SELECT COUNT (Pessoa) FROM Pessoa Pessoa ";
 	
 	@PersistenceContext private EntityManager manager;
 	private Query query;
@@ -85,8 +118,7 @@ public class PessoaRepositoryImplementation implements PessoaRepository{
 
 	@Override
 	public Pessoa findBy(Long codigo) {
-		// TODO Auto-generated method stub
-		return null;
+		return manager.find(Pessoa.class, codigo, LockModeType.PESSIMISTIC_READ);
 	}
 
 	@Override
