bf574c0f3adc3d43ff1e72994390b15f48b41834
Juliano Ramos <jhullyannoors@gmail.com>
2022-07-20 05:59:55 -0300

    criando JPAUtil.java

--- src/main/java/br/com/application/model/motors/Automovel.java
+++ src/main/java/br/com/application/model/motors/Automovel.java
@@ -4,10 +4,14 @@ import java.io.Serializable;
 import java.math.BigDecimal;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
+@Entity
+@Table(name = "automoveis")
 public class Automovel implements Serializable {
 
 	@Id
